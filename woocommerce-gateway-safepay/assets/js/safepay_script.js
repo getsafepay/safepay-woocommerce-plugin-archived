6c231ecb614d0ae5313f10f5f3cdb58592e71165
daniel <dcivit@dharmawebstudio.com>
2019-05-22 00:47:41 +0200

    Delete function that has should not be there in script

--- woocommerce-gateway-safepay/assets/js/safepay_script.js
+++ woocommerce-gateway-safepay/assets/js/safepay_script.js
@@ -81,42 +81,6 @@ function usingGateway(){
     }
 } 
 
-function validate_field() {
-	var $this             = jQuery( this ),
-		$parent           = $this.closest( '.form-row' ),
-		validated         = true,
-		validate_required = $parent.is( '.validate-required' ),
-		validate_email    = $parent.is( '.validate-email' ),
-		event_type        = 'validate';
-
-	if ( 'input' === event_type ) {
-		$parent.removeClass( 'woocommerce-invalid woocommerce-invalid-required-field woocommerce-invalid-email woocommerce-validated' );
-	}
-	if ( 'validate' === event_type || 'change' === event_type ) {
-		if ( validate_required ) {
-			if ( 'checkbox' === $this.attr( 'type' ) && ! $this.is( ':checked' ) ) {
-				$parent.removeClass( 'woocommerce-validated' ).addClass( 'woocommerce-invalid woocommerce-invalid-required-field' );
-				validated = false;
-			} else if ( $this.val() === '' ) {
-				$parent.removeClass( 'woocommerce-validated' ).addClass( 'woocommerce-invalid woocommerce-invalid-required-field' );
-				validated = false;
-			}
-		}
-		if ( validate_email ) {
-			if ( $this.val() ) {
-				var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-				if ( ! pattern.test( $this.val()  ) ) {
-					$parent.removeClass( 'woocommerce-validated' ).addClass( 'woocommerce-invalid woocommerce-invalid-email' );
-					validated = false;
-				}
-			}
-		}
-		if ( validated ) {
-			$parent.removeClass( 'woocommerce-invalid woocommerce-invalid-required-field woocommerce-invalid-email' ).addClass( 'woocommerce-validated' );
-		}
-	}
-}
-
 function isValid() {
 	var validation_s = false;
     var validations = [];
